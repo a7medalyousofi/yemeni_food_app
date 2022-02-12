b200121f116c07da6c4334897402ec12d22ac768
Ahmed Al-Yousofi <s.ahmed.s2013@gmail.com>
2022-02-13 03:37:46 +0530

    Fix footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,17 @@
+import Link from 'next/link'
+
 function Footer() {
   return (
     <div className="border-t border-gray-200 bg-white">
-      <div className="container mx-auto p-4">
+      <div className="container mx-auto flex flex-col items-center space-y-2 p-4 md:flex-row md:justify-between md:space-y-0">
         <p className="text-center text-sm font-medium text-orange-600">
-          جميع الحقوق محفوظة لمنصة المطبخ اليمني {new Date().getFullYear()} ©
+          منصة المطبخ اليمني {new Date().getFullYear()} ©
+        </p>
+        <p className="text-center text-sm font-medium text-orange-600">
+          تصميم وبرمجة{' '}
+          <Link href="https://www.facebook.com/a7medalyousofi/">
+            احمد اليوسفي
+          </Link>
         </p>
       </div>
     </div>
